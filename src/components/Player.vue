b1848ef06990aa08f1b3d75a0556b8e407885b54
Wietse Wind <mail@wietse.com>
2019-10-14 16:59:46 +0200

    Add everything except for MP3 files

---
+++ src/components/Player.vue
@@ -0,0 +1,139 @@
+<template>
+  <div id="player" class="col-12 col-lg-8 col-xl-6" style="margin: 0 auto 0 auto;">
+    <div v-if="readyState">
+      <div id="buttons" v-if="readyState">
+        <div class="text-center mb-4">
+          <button @click="skipBackward" class="btn btn-outline-secondary">
+            <i class="fas fa-backward"></i>
+          </button>
+          <button v-if="paying" @click="pause" class="btn btn-lg btn-outline-danger mx-3">
+            <i class="fas fa-pause"></i>
+          </button>
+          <button v-if="!paying" @click="play" class="btn btn-lg btn-success mx-3">
+            <i class="fas fa-play"></i>
+          </button>
+          <button @click="skipForward" class="btn btn-outline-secondary">
+            <i class="fas fa-forward"></i>
+          </button>
+        </div>
+      </div>
+
+      <aplayer
+        v-if="readyState" ref="aplayer"
+        :audio="audio" :lrcType="0" :mini="false"
+        :autoplay="true"
+        :theme="theme" :loop="'all'" :preload="'auto'"
+        :volume="0.8" :listFolded="false" :listMaxHeight="190"
+        @play="handle"
+        @playing="handle"
+        @progress="handle"
+        @pause="handle"
+        @waiting="handle"
+        @timeupdate="handle"
+        @suspend="handle"
+        @stalled="handle"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { getPaletteFromURL } from 'color-thief-node'
+import * as id3 from 'id3js'
+
+export default {
+  name: 'Player',
+  props: {
+    onReady: Function,
+    onMonetize: Function,
+    readyState: Boolean
+  },
+  components: {
+  },
+  methods: {
+    monetize () {
+      this.onMonetize(this.paying ? this.pointer : null)
+    },
+    handle () {
+      if (arguments[0].type === 'play') {
+        this.paying = true
+        this.pointer = this.$refs.aplayer.currentMusic.payments
+      } else if (arguments[0].type === 'pause') {
+        this.paying = false
+      } else {
+        // console.log(arguments[0].type, arguments[0])
+      }
+    },
+    play () {
+      this.$refs.aplayer.play()
+    },
+    pause () {
+      this.$refs.aplayer.pause()
+    },
+    skipForward () {
+      this.$refs.aplayer.skipForward()
+    },
+    skipBackward () {
+      this.$refs.aplayer.skipBackward()
+    }
+  },
+  watch: {
+    paying () {
+      this.monetize()
+    },
+    pointer () {
+      this.monetize()
+    }
+  },
+  computed: {
+  },
+  async created () {
+    const songs = [
+      require('../assets/1.mp3'),
+      require('../assets/2.mp3'),
+      require('../assets/3.mp3'),
+      require('../assets/4.mp3')
+    ]
+
+    Promise.all(songs.reduce((all, current) => {
+      all.push(new Promise(async (resolve, reject) => {
+        const tags = await id3.fromUrl(current)
+        const coverBlob = new Blob([tags.images[0].data], { type: tags.images[0].mime })
+        const coverUrl = window.URL.createObjectURL(coverBlob)
+        const color = await getPaletteFromURL(coverUrl)
+        resolve({
+          name: tags.title,
+          artist: tags.artist,
+          url: current,
+          cover: coverUrl,
+          theme: `rgb(${color[4].join(',')})`,
+          payments: tags.comments
+        })
+        // console.log(tags.album)
+        // console.log(tags.artist)
+        // console.log(tags.title)
+        // console.log(tags.band)
+        // console.log(tags.genre)
+        // console.log(tags.comments)
+        // console.log(tags.images[0].data)
+      }))
+      return all
+    }, [])).then(ready => {
+      this.theme = ready[0].theme
+      this.audio = ready
+      this.onReady()
+    })
+  },
+  data () {
+    return {
+      paying: false,
+      pointer: '',
+      theme: undefined,
+      audio: undefined
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/Receipt.vue
@@ -0,0 +1,57 @@
+<template>
+  <div v-if="Object.keys(monetizationLog).length > 0" id="receipt" class="mt-5">
+    <h4 class="text-center">Receipt</h4>
+    <div class="alert alert-warning text-center">
+      The payment pointers are stored in the <a href="https://en.wikipedia.org/wiki/ID3" target="_blank"><code>comment</code> field in the ID3 tags</a> 🎉
+    </div>
+    <ul class="list-group">
+      <li v-for="(value, pointer) in monetizationLog" v-bind:key="pointer" class="list-group-item py-1 px-2" :class="{ active: pointer === active }">
+        <div class="float-right">
+          <button v-for="(amount, currency) in value" class="btn btn-light py-0 px-1" :class="{ 'text-dark': pointer === active, 'text-secondary': pointer !== active }" v-bind:key="currency">
+            {{ currency }}
+            <span class="badge" :class="{ 'badge-primary': pointer === active, 'badge-light text-secondary': pointer !== active }">
+              <b>{{ amount.toFixed(5) }}</b>
+            </span>
+          </button>
+        </div>
+        <div class="h6 py-0 pt-1 my-0">
+          <a :href="'https://twitter.com/' + pointer.split('/')[1]" target="_blank" class="d-inline" :class="{ 'text-white': pointer === active }">
+            <i class="fab fa-twitter"></i> {{ pointer }}
+          </a>
+          <small v-if="pointer.match(/WietseWind/)" class="d-none d-sm-inline"> (when paused)</small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Receipt',
+  props: {
+    monetizationLog: Object,
+    render: Number,
+    active: String
+  },
+  components: {
+  },
+  methods: {
+  },
+  watch: {
+    render () {
+      this.$forceUpdate()
+    }
+  },
+  computed: {
+  },
+  created () {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
